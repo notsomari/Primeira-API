f01283dbdbf83b8aa7dbf12369d38d7bdcb566ea
Mariana Miranda <marianamiranda.ms@gmail.com>
2025-03-14 19:46:20 -0300

    Conclusão da API, desta vez totalmente funcional.

--- app.py
+++ app.py
@@ -1,19 +1,55 @@
-from flask import Flask
+import sqlite3
+
+from flask import Flask,request, jsonify
+
 
 app = Flask(__name__) 
 
-@app.route("/pagar")
-def exibir_mensagem():
-    return "<h1>Pagar as pessoas, faz bem as pessoas!!!</h1>"
+def init_db():
+    # sqlite3 cria o arquivo database.db e se conecta com a variável conn (connection)
+    with sqlite3.connect("database.db") as conn:
+        # Executamos um comando SQL para criar a tabela LIVROS, caso ela ainda não exista
+        conn.execute(
+            """ 
+                CREATE TABLE IF NOT EXISTS LIVROS(
+                     id INTEGER PRIMARY KEY AUTOINCREMENT,
+                     titulo TEXT NOT NULL,
+                     categoria TEXT NOT NULL,
+                     autor TEXT NOT NULL,
+                     image_url TEXT NOT NULL
+                )    
+            """
+        )
+        
+init_db()
 
-@app.route("/femandaopix")
-def manda_o_pix():
-    return"<h2>Se a tela apagou, tá devendo!!!</h2>"
+@app.route("/doar", methods =["POST"])
+def doar():
+    
+    dados = request.get_json()
+    
+    print(f"Seus Dados Foram Retornados {dados}")
+    
+    titulo = dados.get("titulo")
+    categoria = dados.get("categoria")
+    autor = dados.get("autor")
+    image_url = dados.get("imagem_url")
 
-@app.route("/comida")
-def comida():
-    return "<h2>Tomato à milanesa</h2>"
+    if not titulo or not categoria or not autor or not image_url:
+        return jsonify({"erro":"Todos os campos são obrigatórios."}), 400
+
+    with sqlite3.connect("database.db") as conn:
+        conn.execute(""""
+        INSERT INTO LIVROS (titulo,categoria,autor,imagem_url)
+        VALUES ("{titulo}", "{categoria}", "{autor}", "{image_url}")
+""")
+
+    conn.commit()
+    
+    return jsonify({"mensagem":"Livro cadastrado com sucesso."}), 201
 
 # Se o arquivo app.py for o arquivo principal da nossa aplicação, rode a api no modo de depuração
 if __name__ == "__main__":
+    # Inicia o servidor Flask no modo de depuração
+    # O modo debug faz com que as mudanças no código sejam aplicadas automaticamente, sem necessidade de 
     app.run(debug=True)
